b6509d459fe9d52be2d3dc64d8e07583469e82db
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-10-17 22:54:02 +0500

    update upload thing ondrop to only accept images without ternary op

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,44 +1,39 @@
-'use client'
+"use client";
 
-import { useCallback, Dispatch, SetStateAction } from 'react'
-import { useDropzone } from '@uploadthing/react/hooks'
-import { generateClientDropzoneAccept } from 'uploadthing/client'
+import { useCallback, Dispatch, SetStateAction } from "react";
+import { useDropzone } from "@uploadthing/react/hooks";
+import { generateClientDropzoneAccept } from "uploadthing/client";
 
-import { Button } from '@/components/ui/button'
-import { convertFileToUrl } from '@/lib/utils'
+import { Button } from "@/components/ui/button";
+import { convertFileToUrl } from "@/lib/utils";
 
 type FileUploaderProps = {
-  onFieldChange: (url: string) => void
-  imageUrl: string
-  setFiles: Dispatch<SetStateAction<File[]>>
-}
+  onFieldChange: (url: string) => void;
+  imageUrl: string;
+  setFiles: Dispatch<SetStateAction<File[]>>;
+};
 
 export function FileUploader({ imageUrl, onFieldChange, setFiles }: FileUploaderProps) {
   const onDrop = useCallback((acceptedFiles: any[]) => {
-    setFiles(acceptedFiles)
-    onFieldChange(convertFileToUrl(acceptedFiles[0]))
-  }, [])
+    setFiles(acceptedFiles);
+    onFieldChange(convertFileToUrl(acceptedFiles[0]));
+  }, []);
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    accept: 'image/*' ? generateClientDropzoneAccept(['image/*']) : undefined,
-  })
+    accept: generateClientDropzoneAccept(["image/*"]),
+  });
 
   return (
     <div
       {...getRootProps()}
-      className="flex-center bg-dark-3 flex h-72 cursor-pointer flex-col overflow-hidden rounded-xl bg-grey-50">
+      className="flex-center bg-dark-3 flex h-72 cursor-pointer flex-col overflow-hidden rounded-xl bg-grey-50"
+    >
       <input {...getInputProps()} className="cursor-pointer" />
 
       {imageUrl ? (
         <div className="flex h-full w-full flex-1 justify-center ">
-          <img
-            src={imageUrl}
-            alt="image"
-            width={250}
-            height={250}
-            className="w-full object-cover object-center"
-          />
+          <img src={imageUrl} alt="image" width={250} height={250} className="w-full object-cover object-center" />
         </div>
       ) : (
         <div className="flex-center flex-col py-5 text-grey-500">
@@ -51,5 +46,5 @@ export function FileUploader({ imageUrl, onFieldChange, setFiles }: FileUploader
         </div>
       )}
     </div>
-  )
+  );
 }
